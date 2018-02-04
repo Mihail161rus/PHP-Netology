fafa9cdd07feae396a550b2bf1d28e23839d59a2
Unknown <mihail-orlenko@mail.ru>
2018-02-04 15:31:14 +0300

    homework-1-3 ver.0.5

--- homework-1-3/index.php
+++ homework-1-3/index.php
@@ -1,6 +1,5 @@
 <?php
-echo '<h3>Задание 1 - задаем массив животных</h3><hr>';
-
+/*Задаем многомерный массив с животными, которые привязаны к континентам*/
 $animalsArray = [
     'Australia' => [
         'Macropus agilis',
@@ -28,69 +27,47 @@ $animalsArray = [
     ]
 ];
 
-echo '<pre>';
-print_r($animalsArray);
-echo '</pre>';
-
-echo '<hr><h3>Задание 2 - новый массив с животными из 2-х слов</h3><hr>';
+/*Создаем многомерный массив, в который помещаем животных из 2-х слов, при этом первое слово помещаем в массив 0, второе - в массив 1*/
+$sepAnimals = [
+    '0' => [],
+    '1' => [],
+];
 
 foreach ($animalsArray as $continent => $animals) {
     foreach ($animals as $animal) {
         $result = str_word_count($animal, 0);
         if ($result == 2) {
-            $newAnimalsArray[] = $animal;
+            $tempArray = explode(' ', $animal);
+            $sepAnimals[0][] = $tempArray[0];
+            $sepAnimals[1][] = $tempArray[1];
         }
     }
 }
 
-echo '<pre>';
-print_r($newAnimalsArray);
-echo '</pre>';
-
-echo '<hr><h3>Задание 3 - случайно перемешайте между собой первые и вторые слова названий животных</h3><hr>';
-
-foreach ($newAnimalsArray as $animal) {
-    $tempArray = explode(' ', $animal);
-    if (count($tempArray) == 2) {
-        $sepAnimals[0][] = $tempArray[0];
-        $sepAnimals[1][] = $tempArray[1];
-    }
-}
-
+/*Функции перемешивания слов*/
 shuffle($sepAnimals[0]);
 shuffle($sepAnimals[1]);
 
-for ($i = 0; $i < count($sepAnimals[0]); $i++) {
-    $shuffleAnimalsArr[] = $sepAnimals[0][$i] . ' ' . $sepAnimals[1][$i];
-}
-
-echo '<pre>';
-print_r($shuffleAnimalsArr);
-echo '</pre>';
-
-echo '<hr><h3>Дополнительное задание</h3><hr>';
-
+/*Функция поиска континента по первому слову животного*/
 function searchContinent($animalFirstWord, $animalsArray) {
     foreach ($animalsArray as $continent => $animals) {
         $result = implode(' ', $animals);
-        if(strpos($result, $animalFirstWord)) {
+        if(is_numeric(strpos($result, $animalFirstWord))) {
             return $continent;
         } 
     }
 }
 
+/*Создаем новый массив с фантазийными животными и выводим континент по первому слову животного*/
+$newAnimalsArray = [];
+
 for ($i=0; $i < count($sepAnimals[0]); $i++) { 
-    $shuffleAnimalsContinent[searchContinent($sepAnimals[0][$i], $animalsArray)][] = $sepAnimals[0][$i] . ' ' . $sepAnimals[1][$i];
+    $newAnimalsArray[searchContinent($sepAnimals[0][$i], $animalsArray)][] = $sepAnimals[0][$i] . ' ' . $sepAnimals[1][$i];
 }
 
-echo '<pre>';
-print_r($shuffleAnimalsContinent);
-echo '</pre>';
-
-foreach ($shuffleAnimalsContinent as $continent => $animals) {
+foreach ($newAnimalsArray as $continent => $animals) {
     echo '<h2>' . $continent . '</h2>';
     $result = implode(', ', $animals);
-    echo $result;
+    echo '<p>' . $result . '</p>';
 }
-
 ?>
