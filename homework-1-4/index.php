3e7dad7ad0dd7f52124a241918b40707a1ab7c0c
Unknown <mihail-orlenko@mail.ru>
2018-02-06 11:21:43 +0300

    ver 0.2

--- homework-1-4/index.php
+++ homework-1-4/index.php
@@ -9,7 +9,9 @@ $humidity = $weatherArray['main']['humidity'];
 $wind_speed = round($weatherArray['wind']['speed'], 1);
 $sunrise = date_sunrise($weatherArray['sys']['sunrise']);
 $sunset = date_sunset($weatherArray['sys']['sunset']);
-
+$icon_weather = $weatherArray['weather'][0]['icon'];
+$weather_desc = $weatherArray['weather'][0]['description'];
+$date_now = date('d M Y - G:i');
 ?>
 
 <!DOCTYPE html>
@@ -17,18 +19,37 @@ $sunset = date_sunset($weatherArray['sys']['sunset']);
 <head>
 	<meta charset="UTF-8">
 	<title>Текущая погода в <?=$city_name?></title>
+    <style>
+        table {
+            border: 1px solid #eeeeee;
+        }
+
+        td {
+            padding: 5px 15px;
+            border: 1px solid #eeeeee;
+        }
+    </style>
 </head>
 <body>
 	<h1>Погода в <?=$city_name?></h1>
 
+    <h3>Текущая дата: <?=$date_now?></h3>
+
 	<table>
+        <tr>
+            <td rowspan="2">Погодные условия</td>
+            <td style="text-align: center;"><img src="http://openweathermap.org/img/w/<?=$icon_weather?>.png" alt="<?=$weather_desc?>"></td>
+        </tr>
+        <tr>
+            <td><?=$weather_desc?></td>
+        </tr>
 		<tr>
 			<td>Температура воздуха</td>
 			<td><?=$temperature?> °C</td>
 		</tr>
 		<tr>
 			<td>Атмосферное давление</td>
-			<td><?=$atmo_pressure?> мм. рт. столба</td>
+			<td><?=$atmo_pressure?> мм. рт. ст.</td>
 		</tr>
 		<tr>
 			<td>Влажность воздуха</td>
