cc54a8e13d8ed1cdaa779cd9b2325c2518436f6d
Unknown <mihail-orlenko@mail.ru>
2018-02-13 00:56:01 +0300

    hw-2-2 ver 0.2

---
+++ hw-2-2/admin.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>Модуль загрузки тестов</title>
+</head>
+<body>
+	<h1>Модуль загрузки тестов</h1>
+	<form method="POST" action="/" enctype="multipart/form-data">
+		
+	</form>
+</body>
+</html>
