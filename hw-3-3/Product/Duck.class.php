627e1d482832f1504b59f9333bab2f30b0751b2d
Mike <mihail-orlenko@mail.ru>
2018-03-16 12:09:41 +0300

    ver 0.9
    Signed-off-by: Mike_Volta <orlenko@voltacom.ru>

---
+++ hw-3-3/Product/Duck.class.php
@@ -0,0 +1,21 @@
+<?php
+namespace Product;
+
+class Duck extends Product
+{
+    protected $category = 'Утка';
+    protected $age;
+
+    public function getTitle()
+    {
+        parent::getTitle();
+        $titleArray = ['Пекинская', 'Московская белая', 'Башкирская'];
+        shuffle($titleArray);
+        return $this->title = $titleArray[0];
+    }
+
+    public function getAge()
+    {
+        return $this->age = rand(2, 4);
+    }
+}
